d6870c8ac1f77ba22e277707d762ecf14f2c3b62
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-17 12:15:23 +0600

    added js code

---
+++ card.js
@@ -0,0 +1,20 @@
+//first card
+document.getElementById('first-card').addEventListener('click', function(){
+   const productName = document.getElementById('first-name').innerText;
+   const productPrice = document.getElementById('first-price').innerText;
+   const productQuantity = document.getElementById('first-quantity').innerText;
+
+   const totalPrice = parseFloat(productPrice) * parseFloat( productQuantity);
+   
+
+   const container = document.getElementById('table-container')
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+        <td>${1}</td>
+        <td>${productName}</td>
+        <td>${productPrice}</td>
+        <td>${productQuantity}</td>
+    
+    `;
+    container.appendChild(tr);
+}); 
